37a3108ee8cc329dba8d6aa01c38fdb481f95ec5
Matheus Rodrigues <matheusrodrigues@poli.ufrj.br>
2022-07-15 14:59:17 -0300

    fix: now the completion menu shows properly

--- nvim/.config/nvim/plugin/colorscheme.lua
+++ nvim/.config/nvim/plugin/colorscheme.lua
@@ -2,7 +2,7 @@ require('onedark').load()
 require('onedark').setup {
 	transparent = true,
     term_colors = true,
-
+    globalstatu = true,
     code_style = {
         comments = 'italic',
         keywords = 'none',
@@ -10,12 +10,10 @@ require('onedark').setup {
         strings = 'none',
         variables = 'none',
     },
-
     diagnostics = {
         darker = true,
         undercurl = false,
         background = false,
     }
-
 }
 vim.cmd[[colorscheme onedark]]
