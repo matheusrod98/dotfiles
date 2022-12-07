a25c19a08ad51d0cafbca44426f4e59b32d5ee79
Matheus Rodrigues <matheusrodrigues@poli.ufrj.br>
2022-12-07 12:30:56 -0300

    feat(install.sh): add zsh plugins

--- install.sh
+++ install.sh
@@ -1,25 +1,19 @@
-# TODO
-# - ssh key.
-# - clone and install dotfiles.
-# - powerlevel10k
-# - packer.nvim
-# - distroboxes
-# - zsh-completions and zsh-fast-syntax-highliting
-
-rpm-ostree install distrobox docker docker-compose
+rpm-ostree install distrobox docker docker-compose bismuth kdeconnectd
 rpm-ostree override remove firefox podman toolbox
 
 flatpak remote-delete fedora
 flatpak remote-delete fedora-testing
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-flatpak install com.brave.Browser com.discordapp.Discord com.heroicgameslauncher.hgl com.obsproject.Studio com.spotify.Client com.stremio.Stremio com.valvesoftware.Steam io.github.mimbrero.WhatsAppDesktop org.freedesktop.LinuxAudio.Plugins.TAP org.freedesktop.LinuxAudio.Plugins.swh org.freedesktop.Platform org.freedesktop.Platform org.freedesktop.Platform.Compat.i386 org.freedesktop.Platform.GL.default org.freedesktop.Platform.GL.default org.freedesktop.Platform.GL32.default org.freedesktop.Platform.ffmpeg-full org.freedesktop.Platform.openh264 org.freedesktop.Platform.openh264 org.gimp.GIMP org.gnome.Platform org.gtk.Gtk3theme.Breeze org.kde.Platform org.kde.Platform org.kde.haruna org.kde.kdenlive org.libreoffice.LibreOffice org.mozilla.firefox org.telegram.desktop org.telegram.desktop.webview
+flatpak install com.brave.Browser com.discordapp.Discord com.getpostman.Postman com.obsproject.Studio com.spotify.Client com.stremio.Stremio io.github.mimbrero.WhatsAppDesktop org.gimp.GIMP org.kde.haruna org.kde.kamoso org.kde.kclock org.kde.kdenlive org.kde.ktorrent org.libreoffice.LibreOffice org.mozilla.firefox org.telegram.desktop rest.insomnia.Insomnia
 
-toolbox create
-toolbox enter
+distrobox create -n Fedora -i registry.fedoraproject.org/fedora-toolbox:37 
+distrobox enter Fedora
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf copr enable atim/lazygit -y
 sudo dnf upgrade -y --refresh
-sudo dnf install neovim git-lfs stow clang gcc nodejs npm ripgrep finder-fd lazygit code docker docker-compose wl-clipboard
+sudo dnf install neovim git-lfs stow clang gcc nodejs npm ripgrep fd-find lazygit code docker docker-compose wl-clipboard
 git-lfs install
 
 git clone git@github.com:matheusrod98/dotfiles.git ~/.dotfiles
@@ -28,6 +22,9 @@ touch ~/.cache/zsh/.zhistory
 mkdir -p ~/.config/npm/npm-global/bin
 mkdir -p ~/.cargo/bin
 mkdir -p ~/.local/share/zsh/plugins
-cd ~/.local/share/zsh/
-
+cd ~/.local/share/zsh/plugins
+git clone https://github.com/z-shell/F-Sy-H
+cd ..
+git clone git clone --depth=1 https://github.com/romkatv/powerlevel10k.git
+cd ~/.dotfiles
 stow *
