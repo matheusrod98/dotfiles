a2fa80e2a9fc2d6061de44f67f065810eed9cba3
Matheus Rodrigues <matheusrodrigues@poli.ufrj.br>
2022-12-14 19:50:48 -0300

    feat(asdf): add asdf tool-versions

--- nvim/.config/nvim/plugin/colorscheme.lua
+++ nvim/.config/nvim/plugin/colorscheme.lua
@@ -1,6 +1,11 @@
 require("tokyonight").setup({
     style = "night",
     light_style = "day",
+    transparent = true,
+    -- styles = {
+    --     sidebars = "transparent",
+    --     floats = "transparent",
+    -- }
 })
 
 vim.cmd[[colorscheme tokyonight]]
