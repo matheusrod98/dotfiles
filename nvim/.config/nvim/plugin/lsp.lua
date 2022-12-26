201edb9d83ac7ede9615a8a5656b3515c506c7e7
Matheus Rodrigues <matheusrodrigues@poli.ufrj.br>
2022-12-26 20:27:37 -0300

    feat(neovim): add diagnostic signs on sign bar

--- nvim/.config/nvim/plugin/lsp.lua
+++ nvim/.config/nvim/plugin/lsp.lua
@@ -72,3 +72,9 @@ local lspkind = require('lspkind')
           })
       }
   }
+
+local signs = { Error = "", Warning = "", Hint = "", Information = "" }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
