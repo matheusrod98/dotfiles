648c397f94eea500b26e04c25ecd25194fe4fe4f
Matheus Rodrigues <matheusrodrigues@poli.ufrj.br>
2022-12-29 01:22:19 -0300

    bunch of stuff

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf copr enable atim/lazygit -y
 sudo dnf upgrade -y --refresh
-sudo dnf install neovim git-lfs stow clang gcc nodejs npm ripgrep fd-find lazygit code docker docker-compose wl-clipboard tmux alacritty zsh
+sudo dnf install neovim git-lfs stow clang gcc nodejs npm ripgrep fd-find lazygit code docker docker-compose wl-clipboard tmux alacritty zsh trash-cli
 git-lfs install
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
