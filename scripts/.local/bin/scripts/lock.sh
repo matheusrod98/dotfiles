76562efba943fb767efcf42ed8f751ff5a1f4a24
Matheus de Souza <matheusrodrigues@poli.ufrj.br>
2023-09-21 21:33:12 -0300

    feat(swhkd): more apps keybindings

--- scripts/.local/bin/scripts/lock.sh
+++ scripts/.local/bin/scripts/lock.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
-# swaylock \
-# 	--screenshots \
-# 	--clock \
-# 	--indicator \
-# 	--indicator-radius 100 \
-# 	--indicator-thickness 7 \
-# 	--effect-blur 7x5 \
-# 	--effect-vignette 0.5:0.5 \
-# 	--fade-in 0.2
+swaylock \
+	--screenshots \
+	--clock \
+	--indicator \
+	--indicator-radius 100 \
+	--indicator-thickness 7 \
+	--effect-blur 8x5 \
+	--effect-vignette 0.5:0.5 \
+	--fade-in 0.2
 
-SCREENSHOT=/tmp/screenshot.png
-rm $SCREENSHOT
-grim $SCREENSHOT
-convert $SCREENSHOT -blur 7x5 $SCREENSHOT
-swaylock -i $SCREENSHOT --indicator-idle-visible
+# SCREENSHOT=/tmp/screenshot.png
+# rm $SCREENSHOT
+# grim $SCREENSHOT
+# convert $SCREENSHOT -blur 7x5 $SCREENSHOT
+# swaylock -i $SCREENSHOT --indicator-idle-visible
