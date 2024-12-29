8bd504ce5368c57032baea7224087c3da0c3ceda
Matheus de Souza <matheusrodrigues@poli.ufrj.br>
2024-12-29 19:27:59 -0300

    fix(fzf): do not exclude *nix* from changeDirWidget

--- nix/home/terminal/fzf.nix
+++ nix/home/terminal/fzf.nix
@@ -4,7 +4,7 @@
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
-    changeDirWidgetCommand = "fd --type d -H -L --exclude '*node_modules*' --exclude '*nix*' --exclude '*.local*' --exclude '*.git*' --exclude '.cache'";
+    changeDirWidgetCommand = "fd --type d -H -L --exclude '*node_modules*' --exclude '*nix-*' --exclude '*.local*' --exclude '*.git*' --exclude '.cache'";
     fileWidgetCommand = "fd --type f --exclude '*node_modules*' --exclude '*nix*' --exclude '*.local*' --exclude '*.git*' --exclude '.cache'";
     defaultCommand = "fd --type f --exclude '*node_modules*' --exclude '*nix*' --exclude '*.local*' --exclude '*.git*' --exclude '.cache'";
   };
