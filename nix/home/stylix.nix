d95bbabab56e20bde35b7e1b4fa8914bd8cffd08
Matheus de Souza <matheusrodrigues@poli.ufrj.br>
2024-12-29 19:35:54 -0300

    fix(stylix): removed duplicated config in hm

--- nix/home/stylix.nix
+++ nix/home/stylix.nix
@@ -1,15 +1,6 @@
 { pkgs, ... }:
 {
   stylix.enable = true;
-  stylix.image = ./../wallpapers/flower.jpg;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
-  stylix.fonts = {
-    monospace = {
-      package = pkgs.jetbrains-mono;
-      name = "JetBrains Mono";
-    };
-    sizes.terminal = 13;
-  };
   stylix.cursor.size = 24;
   stylix.iconTheme = {
     enable = true;
