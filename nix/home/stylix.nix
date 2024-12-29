ef916d876ee17bc6fe94833677e5d6ecc0b0dab9
Matheus de Souza <matheusrodrigues@poli.ufrj.br>
2024-12-29 19:30:24 -0300

    feat(stylix): use default stylix fonts

--- nix/home/stylix.nix
+++ nix/home/stylix.nix
@@ -4,21 +4,9 @@
   stylix.image = ./../wallpapers/flower.jpg;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
   stylix.fonts = {
-    emoji = {
-      package = pkgs.noto-fonts-color-emoji;
-      name = "Noto Color Emoji";
-    };
     monospace = {
       package = pkgs.jetbrains-mono;
       name = "JetBrains Mono";
-    };
-    sansSerif = {
-      package = pkgs.noto-fonts;
-      name = "Noto Sans";
-    };
-    serif = {
-      package = pkgs.noto-fonts;
-      name = "Noto Serif";
     };
     sizes.terminal = 13;
   };
