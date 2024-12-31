67b218c87ebd364042aa1385b1a0825699f48071
Matheus de Souza <matheusrodrigues@poli.ufrj.br>
2024-12-31 03:11:50 -0300

    feat(mime): add new default apps

--- nix/home/mimeapps.nix
+++ nix/home/mimeapps.nix
@@ -1,30 +1,40 @@
 { pkgs, ... }:
 {
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = {
-      "image/jpeg" = [ "imv.desktop" ];
-      "image/png" = [ "imv.desktop" ];
-      "image/webp" = [ "imv.desktop" ];
-      "image/gif" = [ "imv.desktop" ];
-      "image/svg+xml" = [ "imv.desktop" ];
-      "image/svg" = [ "imv.desktop" ];
-      "video/mp4" = [ "mpv.desktop" ];
-      "video/x-matroska" = [ "mpv.desktop" ];
-      "video/x-msvideo" = [ "mpv.desktop" ];
-      "video/x-ms-wmv" = [ "mpv.desktop" ];
-      "x-scheme-handler/http" = [ "firefox.desktop" ];
-      "x-scheme-handler/https" = [ "firefox.desktop" ];
-      "x-scheme-handler/mailto" = [ "firefox.desktop" ];
-      "x-scheme-handler/webcal" = [ "firefox.desktop" ];
-      "application/eps" = [ "org.pwmt.zathura.desktop" ];
-      "application/pdf" = [ "org.pwmt.zathura.desktop" ];
-      "text/plain" = [ "nvim.desktop" ];
-      "application/javascript" = [ "nvim.desktop" ];
-      "application/typescript" = [ "nvim.desktop" ];
-      "x-scheme-handler/postman" = [ "Postman.desktop" ];
-      # "application/x-bittorrent" = [ "transmission-qt.desktop" ];
-      # "x-scheme-handler/magnet" = [ "transmission-qt.desktop" ];
+  xdg = {
+    mime.enable = true;
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "application/x-executable" = [ "ghostty.desktop" ];
+        "image/jpeg" = [ "imv.desktop" ];
+        "image/png" = [ "imv.desktop" ];
+        "image/webp" = [ "imv.desktop" ];
+        "image/gif" = [ "imv.desktop" ];
+        "image/svg+xml" = [ "imv.desktop" ];
+        "image/svg" = [ "imv.desktop" ];
+        "video/mp4" = [ "mpv.desktop" ];
+        "video/x-matroska" = [ "mpv.desktop" ];
+        "video/x-msvideo" = [ "mpv.desktop" ];
+        "video/x-ms-wmv" = [ "mpv.desktop" ];
+        "text/html" = [ "firefox.desktop nvim.desktop" ];
+        "x-scheme-handler/about" = [ "firefox.desktop" ];
+        "x-scheme-handler/http" = [ "firefox.desktop" ];
+        "x-scheme-handler/https" = [ "firefox.desktop" ];
+        "x-scheme-handler/mailto" = [ "firefox.desktop" ];
+        "x-scheme-handler/webcal" = [ "firefox.desktop" ];
+        "application/eps" = [ "org.pwmt.zathura.desktop" ];
+        "application/pdf" = [ "org.pwmt.zathura.desktop firefox.desktop" ];
+        "text/plain" = [ "nvim.desktop" ];
+        "text/x-python" = [ "nvim.desktop;" ];
+        "application/javascript" = [ "nvim.desktop" ];
+        "application/typescript" = [ "nvim.desktop" ];
+        "application/x-shellscript" = [ "nvim.desktop" ];
+        "application/json" = [ "nvim.desktop" ];
+        "application/xml" = [ "nvim.desktop" ];
+        "x-scheme-handler/postman" = [ "Postman.desktop" ];
+        # "application/x-bittorrent" = [ "transmission-qt.desktop" ];
+        # "x-scheme-handler/magnet" = [ "transmission-qt.desktop" ];
+      };
     };
   };
 }
