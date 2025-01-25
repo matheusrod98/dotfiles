4c62fdfd7bb2a192a22c757b4f8aad30242405ed
Matheus de Souza <matheusrodrigues@poli.ufrj.br>
2025-01-25 20:05:17 -0300

    feat(ghostty): remove unnecessary setting

--- nix/home/terminal/ghostty.nix
+++ nix/home/terminal/ghostty.nix
@@ -1,10 +1,9 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   programs.ghostty = {
     enable = true;
     settings = {
-      theme = "rose-pine";
       font-size = 14;
       window-decoration = false;
       mouse-hide-while-typing = true;
