4e921a04370c47eb9da3f91aeff239d8bb211e96
Matheus de Souza <matheusrodrigues@poli.ufrj.br>
2025-02-22 22:15:57 -0300

    feat(terminal): decrease font size

--- nix/home/terminal/ghostty.nix
+++ nix/home/terminal/ghostty.nix
@@ -5,11 +5,12 @@
     enable = true;
     enableZshIntegration = true;
     settings = {
-      font-size = 14;
+      font-size = 13;
       window-decoration = false;
       mouse-hide-while-typing = true;
       confirm-close-surface = false;
       gtk-single-instance = true;
+      # clipboard-copy = false;
     };
   };
 }
