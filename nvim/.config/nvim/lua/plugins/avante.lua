fc2c62f40a61000ce44f46151451cc379f745b81
Matheus de Souza <maths.souza98@gmail.com>
2025-03-21 19:52:53 -0300

    feat(nvim): update avante provider

--- nvim/.config/nvim/lua/plugins/avante.lua
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -5,10 +5,8 @@ return {
     dependencies = {
         "nvim-treesitter/nvim-treesitter", "stevearc/dressing.nvim",
         "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim",
-        "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-        "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-        "zbirenbaum/copilot.lua", -- for providers='copilot'
-        {
+        "nvim-telescope/telescope.nvim", "nvim-tree/nvim-web-devicons",
+        "zbirenbaum/copilot.lua", {
             "HakonHarnes/img-clip.nvim",
             event = "VeryLazy",
             opts = {
@@ -16,7 +14,6 @@ return {
                     embed_image_as_base64 = false,
                     prompt_for_file_name = false,
                     drag_and_drop = {insert_mode = true},
-                    -- required for Windows users
                     use_absolute_path = true
                 }
             }
@@ -31,7 +28,7 @@ return {
         provider = "claude",
         claude = {
             -- endpoint = "https://api.anthropic.com",
-            model = "claude-3-5-sonnet-20241022"
+            model = "claude-3-7-sonnet-20250219"
             -- temperature = 0,
             -- max_tokens = 4096
         },
